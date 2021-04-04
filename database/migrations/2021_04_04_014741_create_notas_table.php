34915b318401dafb502d12312034795d3f6e9b75
TeDev09 <81885006+TeDev09@users.noreply.github.com>
2021-04-03 20:06:23 -0600

    base de datos añadida y funcionando (no principal)

---
+++ database/migrations/2021_04_04_014741_create_notas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->integer('telefono');
+            $table->text('direccion');
+            $table->text('pago');
+            $table->text('usuario');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notas');
+    }
+}
