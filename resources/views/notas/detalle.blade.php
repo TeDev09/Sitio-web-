c812b84143ad2eba0abcc6e653968f0deb36b543
TeDev09 <81885006+TeDev09@users.noreply.github.com>
2021-04-09 22:30:51 -0600

    cmbido la pag de detalle

--- resources/views/notas/detalle.blade.php
+++ resources/views/notas/detalle.blade.php
@@ -1,35 +1,39 @@
 @extends('plantilla')
 @section('contenido')
 <div class="divider"></div>
-<h1 class="center">ID {{$nota->id}}</h1>
+<h1 class="center">Detalle de la nota</h1>
+<h2 class="center">ID {{$nota->id}}</h2>
 <div class="divider"></div>
-<h4 class="center">Detalle de la nota</h4>
 <br>
-<div class="col s6 push-s3">
+<div class="col s12">
     <table style="background-color: #586063; color: black;" data-aos="fade-right">
         <thead>
             <tr>
-            <th>ID</th>
+                <th>ID</th>
                 <th>Nombres</th>
                 <th>Apelidos</th>
                 <th>Telefono</th>
                 <th>Dirección</th>
                 <th>Pago hora</th>
                 <th>Trabajo</th>
-                <th>Descripción</th>
+                <th>Correo</th>
+                <th>Creado el</th>
+                <th>Actualizado el</th>
             </tr>
         </thead>
 
         <tbody>
             <tr>
+                <td>{{$nota->id}}</td>
                 <td>{{$nota->nombre}}</td>
-                <td>{{$nota->descripcion}}</td>
                 <td>{{$nota->apellido}}</td>
                 <td>{{$nota->telefono}}</td>
                 <td>{{$nota->direccion}}</td>
                 <td>{{$nota->pago}}</td>
                 <td>{{$nota->trabajo}}</td>
-                <td>{{$nota->descripcion}}</td>
+                <td>{{$nota->email}}</td>
+                <td>{{$nota->created_at->format('d M Y H:H ')}}</td>
+                <td>{{$nota->updated_at->format('d M Y H:H')}}</td>
             </tr>
         </tbody>
     </table>
